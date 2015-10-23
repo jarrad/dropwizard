b00e3c74aba5ca419325e9349dd576b8d7a68b2e
jarrad giles <jarrad.giles@validic.com>
2015-10-22 22:57:12 -0400

    add JUnit test for an AssetBundle using the AdminEnvironment ServletEnvironment

--- dropwizard-assets/src/test/java/io/dropwizard/assets/AssetsBundleTest.java
+++ dropwizard-assets/src/test/java/io/dropwizard/assets/AssetsBundleTest.java
@@ -1,29 +1,40 @@
 package io.dropwizard.assets;
 
-import com.google.common.io.Resources;
-import io.dropwizard.jetty.setup.ServletEnvironment;
-import io.dropwizard.servlets.assets.AssetServlet;
-import io.dropwizard.servlets.assets.ResourceURL;
-import io.dropwizard.setup.Environment;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.URL;
+
+import javax.servlet.ServletRegistration;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 
-import javax.servlet.ServletRegistration;
-import java.net.URL;
+import com.google.common.io.Resources;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import io.dropwizard.jetty.setup.ServletEnvironment;
+import io.dropwizard.servlets.assets.AssetServlet;
+import io.dropwizard.servlets.assets.ResourceURL;
+import io.dropwizard.setup.AdminEnvironment;
+import io.dropwizard.setup.Environment;
 
 public class AssetsBundleTest {
     private final ServletEnvironment servletEnvironment = mock(ServletEnvironment.class);
     private final Environment environment = mock(Environment.class);
-
+    private final AdminEnvironment adminEnvironment = mock(AdminEnvironment.class);
+    
     private AssetServlet servlet;
     private String servletPath;
 
     @Before
     public void setUp() throws Exception {
+        when(environment.admin()).thenReturn(adminEnvironment);
         when(environment.servlets()).thenReturn(servletEnvironment);
     }
 
@@ -124,6 +135,30 @@ public class AssetsBundleTest {
         assertThat(servlet.getUriPath())
                 .isEqualTo("/what");
     }
+    
+    @Test
+    public void canSupportAdminEnvironment() {
+      
+      runBundle(new AssetsBundle() {
+        protected ServletEnvironment servlets(Environment environment) {
+          return environment.admin();
+        }
+      }, adminEnvironment);
+            
+      assertThat(servletPath)
+        .isEqualTo("/assets/*");
+
+      assertThat(servlet.getIndexFile())
+        .isEqualTo("index.htm");
+
+      assertThat(servlet.getResourceURL())
+        .isEqualTo(normalize("assets"));
+
+      assertThat(servlet.getUriPath())
+        .isEqualTo("/assets");
+      
+      
+    }
 
     private URL normalize(String path) {
         return ResourceURL.appendTrailingSlash(Resources.getResource(path));
@@ -132,8 +167,16 @@ public class AssetsBundleTest {
     private void runBundle(AssetsBundle bundle) {
         runBundle(bundle, "assets");
     }
+    
+    private void runBundle(AssetsBundle bundle, ServletEnvironment servletEnvironment) {
+      runBundle(bundle, "assets", servletEnvironment);
+    }
 
     private void runBundle(AssetsBundle bundle, String assetName) {
+      runBundle(bundle, assetName, servletEnvironment);
+    }
+
+    private void runBundle(AssetsBundle bundle, String assetName, ServletEnvironment servletEnvironment) {
         final ServletRegistration.Dynamic registration = mock(ServletRegistration.Dynamic.class);
         when(servletEnvironment.addServlet(anyString(), any(AssetServlet.class))).thenReturn(registration);
 
